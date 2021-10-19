4a4b58d5e63f26acb878078310a5a68560e65ef9
Osric Wilkinson <gitmoose@fluffypeople.com>
2021-10-19 17:31:49 +0100

    cs2: first commit, infrastructure

---
+++ impls/cs2/step0_repl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace step0_repl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
