d1f85376dbca69211b94a097a62adca7937fa27a
Osric Wilkinson <gitmoose@fluffypeople.com>
2022-04-14 11:53:38 +0100

    step0

--- impls/cs2/step0/Program.cs
+++ impls/cs2/step0/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace uk.osric.mal {
+    public class Program {
+
+        private string Read(string input) {
+            return input;
+        }
+
+        private string Eval(string input) {
+            return input;
+        }
+
+        private string Print(string input) {
+            return input;
+        }
+
+        private string Rep(string input) {
+            return Print(Eval(Read(input)));
+        }
+
+        public static void Main(string[] args) {
+            Program mal = new Program();
+            while (true) {
+                Console.Write("user> ");
+                string? input = Console.ReadLine();
+                if (input != null) {
+                    Console.WriteLine(mal.Rep(input));
+                }
+            }
+        }
+    }
+}
