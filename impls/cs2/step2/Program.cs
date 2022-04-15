6632d87a4e3a4be8abee8de5968a72b75f0b2f33
Osric Wilkinson <gitmoose@fluffypeople.com>
2022-04-15 11:51:01 +0100

    step1 => step2 transition

---
+++ impls/cs2/step2/Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace uk.osric.mal {
+    public class Program {
+
+        private readonly Reader reader = new Reader();
+
+        private MalType Read(string input) {
+            return reader.ReadStr(input);
+        }
+
+        private MalType Eval(MalType input) {
+            return input;
+        }
+
+        private string Print(MalType input) {
+            return Printer.PrStr(input);
+        }
+
+        private string Rep(string input) {
+            return Print(Eval(Read(input)));
+        }
+
+        public static void Main(string[] args) {
+            Program mal = new Program();
+            while (true) {
+                Console.Write("user> ");
+                string? input = Console.ReadLine();
+                if (input != null) {
+                    try {
+                        Console.WriteLine(mal.Rep(input));
+                    } catch (Exception ex) {
+                        Console.Error.WriteLine($"There was a problem {ex.Message}");
+                        FormatException(ex, Console.Error);
+                    }
+                }
+            }
+        }
+
+        private static void FormatException(Exception ex, TextWriter output) {
+            List<Exception> exceptions = new();
+
+            Exception? inner = ex;
+            while (inner != null) {
+                exceptions.Add(inner);
+                inner = ex.InnerException;
+            }
+
+            exceptions.Reverse();
+
+            for (int i = 0; i < exceptions.Count; i += 1) {
+                Exception e = exceptions[i];
+                if (i > 0) {
+                    output.WriteLine("Wrapped by");
+                }
+                output.WriteLine($"{e.GetType().FullName}: {ex.Message}");
+                output.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
